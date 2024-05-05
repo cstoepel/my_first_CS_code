c38b1cfe93f78fd247b1825a70c08068548fd9f7
Claus Stöpel <claus.stoepel@gmail.com>
2024-05-05 20:15:54 +0200

    Prime

---
+++ CS0/Prim/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Prim
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Prime p = new Prime();
+			p.prime();
+		}
+	}
+
+	internal class Prime
+	{
+		int maxPrime = 1000;
+		int[] p = new int[1000];
+
+		public void prime()
+		{
+			p[0] = 2;
+			int n = 0;
+			for (int i = 3; i < maxPrime; i += 2)
+			{
+				bool is_prime = true;
+				for (int j = 0; j <= n; ++j)
+				{
+					if (i % p[j] == 0)
+					{
+						is_prime = false;
+						break;
+					}
+
+				}
+				if (is_prime)
+				{
+					n++;
+					p[n] = i;
+					Console.Write($"{i}\t");
+				};
+
+			}
+		}
+	}
+}
