f551036219971af5994b35d1d0053c47ad04617c
Claus Stöpel <claus.stoepel@gmail.com>
2024-05-13 04:52:14 +0200

    Test unmanaged code

---
+++ CS0/test/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics.Arm;
+
+namespace test;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Class_A a = new Class_A();
+		Class_B b = new Class_B();
+
+		//Base ba = b;
+		Class_A ba = b;
+		ba.print();
+
+		Class_C c = new Class_C();
+		c.Print1();
+		c.Print2();
+	}
+}
+
+class Base
+{
+	public virtual void print() { Console.WriteLine("Base.print"); }
+}
+
+class Class_A: Base
+{
+	public virtual void print() { Console.WriteLine("Class_A.print"); }
+}
+
+class Class_B : Class_A
+{
+	public override void print() { Console.WriteLine("Class_B.print"); }
+}
+
+public interface I1
+{
+	void Print1();
+}
+
+public interface I2:I1
+{
+	void Print2();
+}
+
+public class Base_of_C
+{
+	public void Print() { Console.WriteLine("Base_of_C.Print"); }
+}
+
+public class Class_C: Base_of_C, I2
+{
+	public void Print1() { Console.WriteLine("Class_C.Print1"); }
+	public void Print2() { Console.WriteLine("Class_C.Print2"); }
+}
---
+++ CS1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using Microsoft.Win32.SafeHandles;
+
+namespace ConsoleApp1;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+		Win32 a = new Win32();
+		//a.test();
+		Win32.MessageBox(0, "HALLO CLAUS", "Message", 0);
+
+		 private readonly SafeFileHandle _consolehandle;
+		_consolehandle = CreateFile("CONOUT$", 0x40000000, 0x02, IntPtr.Zero, FileMode.Open, 0, IntPtr.Zero);
+
+	}
+
+
+
+
+
+
+	class Win32
+	{
+		[DllImport("user32.dll", CharSet = CharSet.Unicode)]
+		public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+	[DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+	public static extern IntPtr CreateFile
+	(
+		UInt32 dwDesiredAccess,
+		UInt32 dwShareMode,
+		IntPtr lpSecurityAttributes,
+		UInt32 dwFlags,
+		IntPtr lpScreenBufferData
+	);
+	[DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+		public static extern IntPtr CreateConsoleScreenBuffer
+		(
+			UInt32 dwDesiredAccess,
+			UInt32 dwShareMode,
+			IntPtr lpSecurityAttributes,
+			UInt32 dwFlags,
+			IntPtr lpScreenBufferData
+		);
+		[DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+		public static extern int WriteConsoleOutput(
+			UInt32  hConsoleOutput,
+			IntPtr lpBuffer,
+			UInt32 nNumberOfCharsToWrite,
+			IntPtr lpNumberOfCharsWritten,
+			IntPtr lpReserved
+		);
+	}
+
+}
+
